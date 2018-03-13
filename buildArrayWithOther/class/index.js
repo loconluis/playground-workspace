bb3d9c696a81786be8d2fc7e0ff992cb01c4d697
Luis Locon <luis.locon@gmail.com>
2018-03-13 12:17:22 -0600

    new function to appendANodeToTheTreeStructure

--- buildArrayWithOther/class/index.js
+++ buildArrayWithOther/class/index.js
@@ -1,29 +1,76 @@
 class TreeStructure {
-  constructor (structure = {}, labelAttr="name", childrenAttr="children", idAttr="_id") {
-    this.structure=structure;
-    this.labelAttr=labelAttr;
-    this.childrenAttr=childrenAttr;
+  // constructor (structure = {}, labelAttr="name", childrenAttr="children", idAttr="_id") {
+    constructor ( props = {} ) {
+    // this.structure=structure;
+    // this.labelAttr=labelAttr;
+    // this.childrenAttr=childrenAttr;
+    this.treeData = props.treeData || {}
+    this.structure = props.structure || {}
+    this.labelAttr = props.labelAttr || 'name'
+    this.childrenAttr = props.childrenAttr || 'children'
+    this.idAttr = props.idAttr || '_id'
+    this.iconAttr = props.iconAttr || 'icon'
+    this.treeMapData = this.getMapTree()
   }
-
+  // build a new tree with a the new structure
   mapTree (treeData) {
     return treeData.map((node, i) => {
       let temp = this.mapNode(node);
-      console.log('im temp', temp) 
+      // console.log('im temp', temp) 
       temp[this.childrenAttr] = this.mapTree(temp[this.childrenAttr])
-      console.log('im temp 2', temp)
+      // console.log('im temp 2', temp)
       return temp;
     })
   }
-  
+  // build a new node with the structure define for the Tree
   mapNode (node) {
     var result = {};
     Object.keys(this.structure).map((value) => {
       result[value]=eval(`node.${this.structure[value]}`)
-      console.log('evalValue', eval(`node.${this.structure[value]}`))
+      // console.log('evalValue', eval(`node.${this.structure[value]}`))
     })
     return result;
   }
+  // Append the child to the tree @Node should be a mapped node
+  
 
+  getTreeData () {
+    return this.treeMapData
+  }
+  
+  getMapTree () {
+    return this.mapTree(this.treeData)
+  }
+
+  appendChildToFather (treeData, newChild) {
+    //console.log(treeData);
+    for (let value in treeData) {
+      //console.log('treeData[value]', treeData[value])
+      const textMatch = treeData[value][this.idAttr].includes(newChild.parentId);
+      if (textMatch) {
+        treeData[value][this.childrenAttr].push(newChild);
+        //console.log('was finding', treeData[value]);
+      } else {
+        treeData[value][this.childrenAttr] = this.appendChildToFather(treeData[value][this.childrenAttr], newChild);
+      }
+    }
+    return treeData;
+  }
+
+  // appendChild (node) {
+  //   //console.log(treeData);
+  //   for (let value in this.treeMapData) {
+  //     //console.log('treeData[value]', treeData[value])
+  //     const textMatch = this.treeMapData[value][this.idAttr].includes(node.parentId);
+  //     if (textMatch) {
+  //       this.treeMapData[value][this.childrenAttr].push(node);
+  //       //console.log('was finding', this.treeMapData[value]);
+  //     } else {
+  //       this.treeMapData[value][this.childrenAttr] = this.appendChild(this.treeMapData[value][this.childrenAttr], node);
+  //     }
+  //   }
+  //   return this.treeMapData;
+  // }
 }
 
 module.exports = TreeStructure;
--- buildArrayWithOther/index.js
+++ buildArrayWithOther/index.js
@@ -1,13 +1,29 @@
 const data = require('./data/index')
 const TreeStructure = require('./class')
 
-const laStructure = {
-  hola: 'name',
-  _id: 'id',
-  icon: 'parentId',
-  children: 'children',
+const props = {
+  treeData: data,
+  structure: {
+    _id: 'id',
+    name: 'name',
+    icon: 'parentId',
+    parentId: 'parentId',
+    children: 'children',
+  }
 }
 
-let TreeStr = new TreeStructure(laStructure, "hola");
-let x = TreeStr.mapTree(data)
-console.log('strc console', JSON.stringify(x, undefined, 2))
+const newChild = {
+  id: '1234---mambo--number---5',
+  name: 'Un buen mambito',
+  icon: 'fa-s',
+  parentId: 'c8aff606-1e30-4190-950e-05be6ad1b6ec'
+}
+
+let TreeStr = new TreeStructure(props);
+let x = TreeStr.getTreeData()
+let mappedChild = TreeStr.mapNode(newChild);
+// let newTree = TreeStr.appendChildToFather(x,mappedChild)
+// console.log('new Tree', JSON.stringify(x, undefined, 2))
+// console.log('new Node', JSON.stringify(mappedChild, undefined, 2))
+console.log('tree with append', JSON.stringify(TreeStr.appendChildToFather(x, mappedChild), undefined, 2))
+// console.log('tree with append 2', JSON.stringify(TreeStr.appendChild(mappedChild), undefined, 2))

76954ad79590b18767041ad842644a233d8d1827
loconluis <luis.locon@gmail.com>
2018-03-19 12:29:48 -0600

    function to edit a child on tree.

--- appendAndUpdate/index.js
+++ appendAndUpdate/index.js
@@ -9,7 +9,15 @@ const newChild = {
   children: []
 };
 
+const updateChild = {
+  id: '5141b464-d522-42d3-9abb-4a9a1a219487',
+  name: 'Sahir El buen Hombre',
+  parentId: 'a4019b9d-8816-4803-a3c8-bb927e267684',
+  children: []
+};
 
 
-const y = selector.appendChildToFather(data, newChild);
-console.log('new array', JSON.stringify(y, undefined, 2))
+//const y = selector.appendChildToFather(data, newChild);
+//console.log('new array', JSON.stringify(y, undefined, 2))
+const z = selector.updateChild(data, updateChild)
+console.log('new array', JSON.stringify(z, undefined, 2))
--- appendAndUpdate/manageTree/index.js
+++ appendAndUpdate/manageTree/index.js
@@ -1,5 +1,5 @@
 const appendChildToFather = (treeData, newChild) => {
-  console.log(treeData);
+  // console.log(treeData);
   for (let value in treeData) {
     console.log('treeData[value]', treeData[value])
     const textMatch = treeData[value].id.includes(newChild.parentId);
@@ -13,4 +13,20 @@ const appendChildToFather = (treeData, newChild) => {
   return treeData;
 }
 
-module.exports = { appendChildToFather }
+const updateChild = (treeData, update) => {
+  for (let value in treeData) {
+    console.log('update id', update.id);
+    console.log('treeData[value].id', treeData[value].id);
+    const idMatch = treeData[value].id.includes(update.id);
+    console.log('idMatch', idMatch);
+    if (idMatch) {
+      treeData[value] = {...update};
+      console.log('after if', treeData[value])
+    } else {
+      treeData[value].children = updateChild(treeData[value].children, update)
+    }
+  }
+  return treeData;
+}
+
+module.exports = { appendChildToFather, updateChild }
